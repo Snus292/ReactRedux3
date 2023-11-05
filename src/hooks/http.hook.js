e9d7810cf2f8cb9fbb7653ff7552337f50f57c90
Snus292 <georgiigiblov@gmail.com>
2023-11-05 18:42:52 +0200

    Добавленны новые герои на json сервере(советуется к просмотру)

--- src/hooks/http.hook.js
+++ src/hooks/http.hook.js
@@ -1,14 +1,21 @@
+// упрощает выполнение HTTP-запросов
+
+
 import { useCallback } from "react";
+// позволяет создавать мемоизированные функции, 
+// которые не будут пересоздаваться при каждом рендеринге компонента.
 
 export const useHttp = () => {
     // const [process, setProcess] = useState("waiting");
 
 
+    // выполняет HTTP-запрос
     const request = useCallback(async (url, method = "GET", body = null, headers = {"Content-Type": "application/json"}) => {
         // setProcess("loading");
 
         try {
             const response = await fetch(url, {method, body, headers});
+            // данные парсятся и возвращаются
 
             if (!response.ok) {
                 throw new Error(`Could not fetch ${url}, status: ${response.status}`);
